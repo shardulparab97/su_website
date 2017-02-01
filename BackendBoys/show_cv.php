c3936553f25f7febaa7c316b6390b37893d5828c
shardulparab97 <shardulSVparab@gmail.com>
2017-02-01 11:28:48 +0530

    Added download cv as pdf functionality

--- BackendBoys/show_cv.php
+++ BackendBoys/show_cv.php
@@ -5,10 +5,20 @@ $id=$_GET['id'];
 //echo $id;
 $query = "SELECT * from student_data WHERE id='$id'";
 $r = @mysqli_query($dbc, $query);
-echo gettype($r);
+//echo gettype($r);
 while($row=mysqli_fetch_array($r,MYSQLI_ASSOC)) {
-echo $row['id'];
-    echo $row['first_name'];
+    echo 'ID:'.$row['id'].'<br>';
+    echo 'First Name:'.$row['first_name'].'<br>';
+    echo 'Last Name:'.$row['last_name'].'<br>';
+    echo 'Branch:'.$row['branch'].'<br>';
+    echo 'Objective:'.$row['objective'].'<br>';
+    echo 'Clubs:'.$row['clubs'].'<br>';
+    echo 'Departments: '.$row['departments'].'<br>';
 
+    echo '<a href="pdfstuff.php?id='.$row['id'].'"><button>Download the CV as pdf</button></a>';
 }
+
+
 ?>
+
+
